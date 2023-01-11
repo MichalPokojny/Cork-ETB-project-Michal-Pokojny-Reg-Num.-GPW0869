0ca02e95bffb6530fc5cd70d21109abd1246590f
MichalPokojny <michal.pokojny@hotmail.com>
2023-01-11 18:06:43 +0000

    Update map.html add geolocation to calcualte distance from user to Dublin

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,42 @@
     let chapter = document.getElementById("seasons-text");
    chapter.innerHTML = "My rating of this restaurant: 8/10";
  }
+
+//  Map function
+
+function clicked () {
+    alert("Great you clicked on the map, let me redirect you to wikipedia.");
+}
+
+function getDistance() {
+  if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+          let lat1 = position.coords.latitude;
+          let lon1 = position.coords.longitude;
+          let lat2 = 53.3498; // Dublin lat
+          let lon2 = 6.2603; // Dubkin lon
+          calcCrow(lat1, lon1, lat2, lon2);
+      });
+  } else {
+      alert("Sorry, your browser does not support HTML5 geolocation.");
+  }
+}
+
+function calcCrow(lat1, lon1, lat2, lon2) 
+    {
+      let R = 6371; // km
+      let dLat = toRad(lat2-lat1);
+      let dLon = toRad(lon2-lon1);
+
+      let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+      let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      let d = (R * c/ 1000).toFixed(2);
+      document.getElementById("result").innerHTML = "Your distance from Dublin is " + d + " km.";
+    }
+
+    // Converts numeric degrees to radians
+    function toRad(Value) 
+    {
+        return Value * Math.PI / 180;
+    }
