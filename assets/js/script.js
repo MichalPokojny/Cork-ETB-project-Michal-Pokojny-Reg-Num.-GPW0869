cfe888a9bec330e4549deda8ee543597cc2da0e2
MichalPokojny <michal.pokojny@hotmail.com>
2023-01-12 15:46:42 +0000

    update distance function for calculating distance to Dublin from user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Food.html image transitions functions
-
-         
+       
  document.getElementById("chapter-one").addEventListener("transitionend", textTransition1);
  document.getElementById("marker").addEventListener("transitionend", textTransition2);
  document.getElementById("locks").addEventListener("transitionend", textTransition3);
@@ -13,60 +12,65 @@
  }
 
  function textTransition2() {
-    let chapter = document.getElementById("marker-text");
-   chapter.innerHTML = "My rating of this restaurant: 10/10";
+    let marker = document.getElementById("marker-text");
+   marker.innerHTML = "My rating of this restaurant: 10/10";
  }
 
  function textTransition3() {
-    let chapter = document.getElementById("locks-text");
-   chapter.innerHTML = "My rating of this restaurant: 8/10";
+    let locks = document.getElementById("locks-text");
+    locks.innerHTML = "My rating of this restaurant: 8/10";
  }
 
  function textTransition4() {
-    let chapter = document.getElementById("saddle-text");
-   chapter.innerHTML = "My rating of this restaurant: 9/10";
+    let saddle = document.getElementById("saddle-text");
+    saddle.innerHTML = "My rating of this restaurant: 9/10";
  }
 
  function textTransition5() {
-    let chapter = document.getElementById("seasons-text");
-   chapter.innerHTML = "My rating of this restaurant: 8/10";
+    let seasons = document.getElementById("seasons-text");
+    seasons.innerHTML = "My rating of this restaurant: 8/10";
  }
 
-//  Map function
+//  Map functions
+
+//  Function for message before redirect to wikipedia
 
 function clicked () {
     alert("Great you clicked on the map, let me redirect you to wikipedia.");
 }
 
+//  Function for calculating distance from user to Dublin implementing haversine method
+
 function getDistance() {
   if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-          let lat1 = position.coords.latitude;
-          let lon1 = position.coords.longitude;
-          let lat2 = 53.3498; // Dublin lat
-          let lon2 = 6.2603; // Dubkin lon
-          calcCrow(lat1, lon1, lat2, lon2);
+          
+          let userPoint = { lat: position.coords.latitude, lng: position.coords.longitude };
+          let dublinPoint = { lat: 53.3498, lng: -6.2603};
+          let haversine_m =  haversineDistance(userPoint, dublinPoint);
+          let haversine_km = (haversine_m /1000).toFixed(0);
+
+          document.getElementById("result").innerHTML = "Your distance from Dublin is " + haversine_km + " km.";  
       });
   } else {
       alert("Sorry, your browser does not support HTML5 geolocation.");
   }
 }
 
-function calcCrow(lat1, lon1, lat2, lon2) 
-    {
-      let R = 6371; // km
-      let dLat = toRad(lat2-lat1);
-      let dLon = toRad(lon2-lon1);
+function squared (x) { return x * x };
+function toRad (x) { return x * Math.PI / 180.0 };
+function hav (x) {
+  return squared(Math.sin(x / 2))
+};
 
-      let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-      let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-      let d = (R * c/ 1000).toFixed(2);
-      document.getElementById("result").innerHTML = "Your distance from Dublin is " + d + " km.";
-    }
+// hav(theta) = hav(bLat - aLat) + cos(aLat) * cos(bLat) * hav(bLon - aLon)
+function haversineDistance (a, b) {
+  const R = 6378137; // equatorial mean radius of Earth (in meters)
+  const aLat = toRad(Array.isArray(a) ? a[1] : a.latitude || a.lat)
+  const bLat = toRad(Array.isArray(b) ? b[1] : b.latitude || b.lat)
+  const aLng = toRad(Array.isArray(a) ? a[0] : a.longitude || a.lng || a.lon)
+  const bLng = toRad(Array.isArray(b) ? b[0] : b.longitude || b.lng || b.lon)
 
-    // Converts numeric degrees to radians
-    function toRad(Value) 
-    {
-        return Value * Math.PI / 180;
-    }
+  const ht = hav(bLat - aLat) + Math.cos(aLat) * Math.cos(bLat) * hav(bLng - aLng)
+  return 2 * R * Math.asin(Math.sqrt(ht))
+};
