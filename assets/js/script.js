f681442b517d6e4087d3412dba76e93df10d084b
MichalPokojny <michal.pokojny@hotmail.com>
2023-01-18 17:30:41 +0000

    text position fix in events.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@
 // XML http request to get list of data to food.html from restaurants.txt
  
 function loadXMLDoc() {
- var xhttpReq = new XMLHttpRequest();
+ let xhttpReq = new XMLHttpRequest();
  
  xhttpReq.onreadystatechange = function() {
      if (this.readyState == 4 && this.status == 200) {
